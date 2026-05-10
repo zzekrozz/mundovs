88534f58c6a033e2d3a164c652a4026972f67ca3
zzekrozz <pogrebnyakivan123@gmail.com>
2026-05-10 08:47:11 +0200

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,25 @@
 import "../styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-883JPQGZ1H"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-883JPQGZ1H');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
