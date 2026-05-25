724403bc48e1e769b0b2c98d59022e8687a1b312
zzekrozz <pogrebnyakivan123@gmail.com>
2026-05-25 18:07:39 +0200

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,9 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="es">
       <Head>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-4516607621229822"
+        />
+
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png" />
@@ -11,6 +16,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192.png" />
         <link rel="icon" type="image/png" sizes="512x512" href="/favicon-512.png" />
       </Head>
+
       <body>
         <Main />
         <NextScript />
